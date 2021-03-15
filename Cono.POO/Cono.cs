24bedd8d595fb26c51f89c5a61082b059653915b
Tadeoferreyra14 <tadeoferreyra14@gmail.com>
2021-03-15 17:51:48 -0300

    Ejercicios02Practico02

---
+++ Cono.POO/Cono.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicios02Practico02.POO
+{
+    public class Cono
+    {
+        public double altura { get; set; }
+        public double generatriz { get; set; }
+        public double radiodegiro { get; set; }
+
+        public double GetArea()
+        {
+            return Math.PI* radiodegiro *(radiodegiro + generatriz);
+        }
+        public double GetVolumen()
+        {
+            return Math.PI* Math.Pow(radiodegiro, 2) * altura / 3;
+        }
+    }
+}
---
+++ Ejercicios02Practico02/Program.cs
@@ -0,0 +1,44 @@
+﻿using Ejercicios02Practico02.POO;
+using System;
+
+namespace Ejercicios02Practico02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cono cono = new Cono();
+                Console.WriteLine("Ingrese el valor de generatriz");
+                var generatriz = double.Parse(Console.ReadLine());
+                Console.WriteLine("ingrese la altura");
+                var altura = double.Parse(Console.ReadLine());
+                Console.WriteLine("ingrese el radio de giro ");
+                var radiodegiro = double.Parse(Console.ReadLine());
+                if(generatriz > 0 & altura > 0 & radiodegiro > 0 )
+                {
+                    cono.generatriz = generatriz;
+                    cono.altura = altura;
+                    cono.radiodegiro = radiodegiro;
+
+                    Console.WriteLine($"El Volumen de un cono es {cono.GetVolumen()} ");
+                    Console.WriteLine($"El Area de un cono es {cono.GetArea()} ");
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un numero mayor a 0");
+                }
+              
+
+                
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Valores invalidos");
+            }
+            Console.ReadLine();
+        }
+    }
+}
